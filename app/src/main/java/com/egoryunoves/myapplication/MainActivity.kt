3059b8f9afffdd81b701350cd56edee3794aa584
EvgenyGoryunov <ges1987@list.ru>
2022-12-11 19:18:33 +0700

    6п - пункт нее сделан полностью, так синтетики были удалены и больше не поддерживаются в новых версиях котлина 6п - частично сделан (название кнопок + прозрачность) ПРОПАЛО название сайта!!! Исправить 5п - заливка в кнопки сделана 4п - нижняя ссылка сделана 3п - картинки сделаны 2п - кнопки сделаны

--- app/src/main/java/com/egoryunoves/myapplication/MainActivity.kt
+++ app/src/main/java/com/egoryunoves/myapplication/MainActivity.kt
@@ -2,10 +2,35 @@ package com.egoryunoves.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+//import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        initButtons()
     }
+
+//    private fun initButtons() {
+//        b_menu.setOnClickListener {
+//            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+//        }
+//        b_izbran.setOnClickListener {
+//            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+//        }
+//        b_posmotr.setOnClickListener {
+//            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+//        }
+//        b_podborki.setOnClickListener {
+//            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+//        }
+//        b_nastroiki.setOnClickListener {
+//            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+//        }
+//    }
+
 }
